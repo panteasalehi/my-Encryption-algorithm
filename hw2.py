07fced641f5efefc6fdeb53c088793761c765b4a
pantea salehi <79785019+panteasalehi@users.noreply.github.com>
2024-06-11 22:23:03 +0330

    Update and rename t3_q2.py to hw2.py

--- hw2.py
+++ hw2.py
@@ -1,12 +1,12 @@
 key_48 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47,
-            55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
+          55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
 ip = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
       62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
       57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3,
       61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7]
 
 exp_48 = [32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20,
-         21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1]
+          21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1]
 
 per = [16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11,
        4, 25]
@@ -54,8 +54,8 @@ def encrypt(pt, rkb):
     left = pt[0:32]
     right = pt[32:64]
     for i in range(16):
-        right_e = permute(right, exp_48, 48)
-        xorx = xor_a_b(right_e, rkb[i])
+        left_e = permute(left, exp_48, 48)
+        xorx = xor_a_b(left_e, rkb[i])
         substition_boxes_str = ""
         for j in range(1, 9):
             row = int(xorx[j - 1]) * int(xorx[(j - 1) * 1 + 2])
@@ -65,12 +65,7 @@ def encrypt(pt, rkb):
             column = column % (row + 1)
             column = int(str(column), 2)
             substition_boxes_str += binaryBuilder(substition_boxes[row][column])
-        left = xor_a_b(left, permute(substition_boxes_str, per, 32))
-
-        if (i != 15):
-            temp = left
-            left = right
-            right = temp
+        right = xor_a_b(right, permute(substition_boxes_str, per, 32))
 
     return permute(left + right, final_p, 64)
 
